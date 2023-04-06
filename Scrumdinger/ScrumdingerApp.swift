2cb1c2fd2c5112d5ec36e807d21e4673cb3a564a
Ivo Coumans <1718078+ivocoumans@users.noreply.github.com>
2023-04-06 13:24:36 +0200

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Ivo Coumans on 06/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
